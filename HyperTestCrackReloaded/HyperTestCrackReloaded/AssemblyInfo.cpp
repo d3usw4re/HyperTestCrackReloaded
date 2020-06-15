a2ebae6a0993540fc0790e7e6b899782ac3c01e7
D3USW4RE <dr.hejam@gmail.com>
2020-06-15 16:25:30 +0300

    Hot fixes

--- HyperTestCrackReloaded/HyperTestCrackReloaded/AssemblyInfo.cpp
+++ HyperTestCrackReloaded/HyperTestCrackReloaded/AssemblyInfo.cpp
@@ -6,30 +6,14 @@ using namespace System::Runtime::CompilerServices;
 using namespace System::Runtime::InteropServices;
 using namespace System::Security::Permissions;
 
-//
-// Allgemeine Informationen über eine Assembly werden über die folgenden
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
-//
-[assembly:AssemblyTitleAttribute(L"CppCLR_WinformsProjekt")];
+[assembly:AssemblyTitleAttribute(L"")];
 [assembly:AssemblyDescriptionAttribute(L"")];
 [assembly:AssemblyConfigurationAttribute(L"")];
 [assembly:AssemblyCompanyAttribute(L"")];
-[assembly:AssemblyProductAttribute(L"CppCLR_WinformsProjekt")];
-[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2017")];
+[assembly:AssemblyProductAttribute(L"")];
+[assembly:AssemblyCopyrightAttribute(L"")];
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
-
-//
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder für die Revisions- und Buildnummer den Standard
-// übernehmen, indem Sie "*" eingeben:
 
 [assembly:AssemblyVersionAttribute("1.0.*")];
 
--- HyperTestCrackReloaded/HyperTestCrackReloaded/stdafx.cpp
+++ HyperTestCrackReloaded/HyperTestCrackReloaded/stdafx.cpp
@@ -1,7 +1,1 @@
-// stdafx.cpp : Quelldatei, die nur die Standard-Includes einbindet.
-// CppCLR_WinformsProjekt.pch ist der vorkompilierte Header.
-// stdafx.obj enthält die vorkompilierten Typinformationen.
-
 #include "stdafx.h"
-
-
